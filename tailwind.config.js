9716ccf65c2adc2e4c011219892e09b103bf9f61
usamasadiqus <usamasadiq029@gmail.com>
2023-09-01 02:26:09 +0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        rainy: "url('/src/assets/images/rainy.jpg')",
-        cloudy: "url('/src/assets/images/cloudy.jpg')",
-        sunny: "url('/src/assets/images/sunny.jpg')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
